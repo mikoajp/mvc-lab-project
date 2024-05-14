699013ddcc5e0f068757b176ed5c88f4f063dc95
Mikoaj Przybysz <mprzybysz@digitalvirgo.com>
2024-05-14 16:57:04 +0200

    feat(mongoose) deprecated fixed

--- src/app.js
+++ src/app.js
@@ -4,30 +4,25 @@ const examRoutes = require('./routes/exam');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-const indexRoutes = require('../src/routes/index');
-app.use('/', indexRoutes);
-
-mongoose.connect("mongodb+srv://mikoajmicht:ixN3yuRGDzvdwUN8@cluster0.8oo6aml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://mikoajmicht:ixN3yuRGDzvdwUN8@cluster0.8oo6aml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use('/', subjectRoutes);
 app.use('/', examRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Serwer działa na porcie ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Click to open: http://127.0.0.1:${PORT}`);
 });
