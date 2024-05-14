82afd9c6806ed099df04a473b5fa003006a6183a
Mikoaj Przybysz <mprzybysz@digitalvirgo.com>
2024-05-14 17:06:10 +0200

    feat(views): path views added

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
-
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home');
 });
